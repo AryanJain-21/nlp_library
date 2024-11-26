00a9d2da9deba3c1b335c0efffd32677cdbfbc2e
Aryan Jain <jain.aryan1@northeastern.edu>
2024-11-25 20:02:47 -0500

    custom parser functionality

--- textastic_app.py
+++ textastic_app.py
@@ -5,19 +5,19 @@ import pprint as pp
 def main():
 
     tt = Textastic()
-    tt.load_text('data/Energy_Sector.json')
-    tt.load_text('data/Healthcare.json')
-    tt.load_text('data/Investment_Finance.json')
-    tt.load_text('data/Retail.json')
-    tt.load_text('data/Technology.json')
-    tt.load_text('data/Other.json')
+    tt.load_text(filename='data/Energy_Sector.json', label='Energy', parser=tp.custom_parser)
+    tt.load_text(filename='data/Healthcare.json', label='Healthcare', parser=tp.custom_parser)
+    tt.load_text(filename='data/Investment_Finance.json', label='Finance', parser=tp.custom_parser)
+    tt.load_text(filename='data/Retail.json', label='Retail', parser=tp.custom_parser)
+    tt.load_text(filename='data/Technology.json', label='Tech', parser=tp.custom_parser)
+    tt.load_text(filename='data/Other.json', label='Other', parser=tp.custom_parser)
 
     print(tt.data)
 
-    #tt.compare_num_words()
-    #tt.wordcount_sankey(word_list= ['customers'], k=5)
+    tt.compare_num_words()
+    tt.wordcount_sankey(k=8)
     tt.wordcloud_subplots(cols=3)
-    tt.heatmap_word_frequencies(["innovation", "customers", "improve", "services", "products", "world", "solutions", "communities"])
+    tt.heatmap_word_frequencies(["clients", "customers", "improve", "services", "products", "world", "solutions", "communities"])
 
 
 if __name__ == '__main__':
